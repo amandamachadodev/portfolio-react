adbf7a68943af1db5b8d6d9b9f9ebd88c9f8bc5c
mandioquynha <machado.amandag@gmail.com>
2023-04-22 15:12:59 -0300

    feat: :sparkles: adicionado cards de repositórios da api do github

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Repositories } from '../Components/Repositories/Repositories';
+
+function Home() {
+    return(
+        <div>
+            <Repositories />
+        </div>
+    );
+}
+
+export default Home;
