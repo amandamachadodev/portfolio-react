dc180826bd31552007c7c4dcb14c169a4b0803e7
mandioquynha <machado.amandag@gmail.com>
2023-05-14 11:02:42 -0300

    base

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,9 +1,13 @@
+/* eslint-disable react/style-prop-object */
 import React from "react";
+import "./Loader.css";
 
 function Loader() {
   return(
-    <div>
-        Loader
+    <div className="caixa">
+        <div className="loader">
+            <span style="--i:1"></span>
+        </div>
     </div>
   );
 }
