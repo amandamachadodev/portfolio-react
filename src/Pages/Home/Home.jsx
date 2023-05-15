b77867b68ff00cc8ef9999b57851805838268efb
mandioquynha <machado.amandag@gmail.com>
2023-05-15 17:00:55 -0300

    feat: :sparkles: adicionado timer do loader e sobre mim na página home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-// import { Repositories } from '../../Components/Repositories/Repositories';
+import React,  { useEffect, useState }  from 'react';
 import { Galaxy } from '../Galaxy/Galaxy.jsx';
 import "../Home/Home.css";
 import { Loader } from '../../Components/Loader/Loader.jsx';
 import { Header } from '../../Components/Header/Header.jsx';
 
 function Home() {
+    const [loader, setLoader] = useState(true);
+
+    const sobreMim = `Olá, meu nome é Amanda. Sou apaixonada por tecnologia desde os 10 anos, idade em que eu ganhei meu primeiro computador.
+    Atualmente estou no ultimo semestre da faculdade de Análise e Desenvolvimento de Sistemas (uihuuuuu!). Paralelo a faculdade fiz um curso tecnico
+    de 'Desenvolvimento web full stack + ciência da computação' na Trybe para me especializar na área de Desenvolvimento de Software. Amo css + react.js e modelar dados + sql.
+    Nas horas vagas eu gosto de assitir séries, cantar, tocar teclado, jogar xadrez e games variados!`
+
+    useEffect(() => {
+    }, [loader])
+
+    setTimeout(() => {
+        setLoader(false);
+      }, 3000);
+
     return(
         <div className="container">
             <Header />
             <Galaxy />
-            < Loader />
+            {!loader && (<p className="sobre">{sobreMim}</p>) } 
+           {loader && < Loader />}
         </div>
     );
 }
